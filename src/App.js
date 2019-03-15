2a814466f25cb4336df7bea5e1bf86634dd79b2d
Mikolaj Torz <mptorz@icloud.com>
2019-03-15 15:57:36 +0000

    implemented App.js to use Hooks instead of class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import front from "./front.svg";
 import back from "./back.svg";
 import ReactCardFlip from "react-card-flip";
@@ -7,49 +7,39 @@ import Div100vh from "react-div-100vh";
 const Front = () => <img className="App-logo" alt="" src={front} />;
 const Back = () => <img className="App-logo" alt="" src={back} />;
 
-class App extends Component {
-  state = {
-    isFlipped: false,
-    neverFlipped: true
-  };
+export default () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [neverFlipped, setNeverFlipped] = useState(true);
 
-  componentDidMount() {
-    const { neverFlipped } = this.state;
-    setTimeout(() => {
-      if (neverFlipped) this.flip(null);
-    }, 2137);
-  }
-
-  flip = e => {
+  const flip = e => {
     if (e) e.preventDefault();
-    this.setState(prevState => ({
-      isFlipped: !prevState.isFlipped,
-      neverFlippd: false
-    }));
+    setNeverFlipped(false);
+    setIsFlipped(!isFlipped);
   };
 
-  render() {
-    const { isFlipped } = this.state;
-    return (
-      <Div100vh className="App">
-        <div
-          onTouchMove={e => e.preventDefault()}
-          onMouseEnter={this.flip}
-          onClick={this.flip}
-        >
-          <ReactCardFlip
-            flipSpeedFrontToBack={2.5}
-            flipSpeedBackToFront={2.5}
-            infinite={true}
-            isFlipped={isFlipped}
-          >
-            <Front key="front" />
-            <Back key="back" />
-          </ReactCardFlip>
-        </div>
-      </Div100vh>
-    );
-  }
-}
+  useEffect(() => {
+    setTimeout(() => {
+      if (neverFlipped) flip(null);
+    }, 2137);
+  });
 
-export default App;
+  return (
+    <Div100vh className="App">
+      <div
+        onTouchMove={e => e.preventDefault()}
+        onMouseEnter={flip}
+        onClick={flip}
+      >
+        <ReactCardFlip
+          flipSpeedFrontToBack={2.5}
+          flipSpeedBackToFront={2.5}
+          infinite={true}
+          isFlipped={isFlipped}
+        >
+          <Front key="front" />
+          <Back key="back" />
+        </ReactCardFlip>
+      </div>
+    </Div100vh>
+  );
+};
